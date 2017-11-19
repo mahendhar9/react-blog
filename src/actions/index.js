a7508b3e4e103c7b09641afa14052af77cc351f2
Mahendhar <mahendarreddy@live.com>
2017-11-19 18:01:58 +0530

    Show all posts at index route (/)

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+export const FETCH_POSTS = 'fetch_posts'
+
+const ROOT_URL = `http://reduxblog.herokuapp.com/api`
+const API_KEY = '?key=mahi8341'
+
+export function fetchPosts() {
+  const url = `${ROOT_URL}/posts${API_KEY}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  }  
+}
+
+export default fetchPosts;
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import _ from "lodash";
+import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id} >
+          {post.title}
+        </li>
+      )
+    });
+  }
+  
   render() {
     return (
       <div>
         All Posts
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+  return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
