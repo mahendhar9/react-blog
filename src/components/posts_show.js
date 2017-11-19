33db857e33fa267c3bc5c98dd27f743599816f36
Mahendhar <mahendarreddy@live.com>
2017-11-20 01:24:03 +0530

    Delete post and redirect to index page

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
-import { fetchPost } from "../actions";
+import { fetchPost, deletePost } from "../actions";
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
   
   render() {
@@ -19,6 +26,9 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back to Index</Link>
+        <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
         <h2>{post.title}</h2>
         <b>Categories:</b> {post.categories}
         <p>{post.content}</p>
@@ -31,4 +41,4 @@ function mapStateToProps({posts}, ownProps) {
   return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
